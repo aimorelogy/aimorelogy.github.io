7361d0d26de1dbf59d390372093553f7623e2306
kieranradxa <keyiran@radxa.com>
2025-06-18 17:28:42 +0800

    update seo setting

---
+++ seo.js
@@ -0,0 +1,42 @@
+(function () {
+  const defaults = {
+    siteName: 'AIMORELOGY - Open Source Incubator',
+    siteUrl: 'https://aimorelogy.github.io',
+    description: 'Leading open source project incubator transforming open source innovations into market success while respecting creators.',
+    image: '/static/logo.png',
+    themeColor: '#00ff88',
+    twitterCard: 'summary_large_image'
+  };
+
+  const page = window.PAGE_SEO || {};
+  const title = page.title || defaults.siteName;
+  const description = page.description || defaults.description;
+  const image = page.image || defaults.image;
+  const url = page.url || (defaults.siteUrl + location.pathname);
+
+  document.title = title;
+
+  function applyMeta(attr, name, content) {
+    if (!content) return;
+    let selector = `[${attr}="${name}"]`;
+    let tag = document.head.querySelector(selector);
+    if (!tag) {
+      tag = document.createElement('meta');
+      tag.setAttribute(attr, name);
+      document.head.appendChild(tag);
+    }
+    tag.setAttribute('content', content);
+  }
+
+  applyMeta('name', 'description', description);
+  applyMeta('name', 'theme-color', defaults.themeColor);
+  applyMeta('property', 'og:title', title);
+  applyMeta('property', 'og:description', description);
+  applyMeta('property', 'og:type', 'website');
+  applyMeta('property', 'og:url', url);
+  applyMeta('property', 'og:image', image);
+  applyMeta('name', 'twitter:card', defaults.twitterCard);
+  applyMeta('name', 'twitter:title', title);
+  applyMeta('name', 'twitter:description', description);
+  applyMeta('name', 'twitter:image', image);
+})(); 
